a26964595761c49d20159b26d2dd4ca09a6bb70f
Nehoxa <loirejulien@outlook.fr>
2023-09-29 11:14:19 +0200

    fix text

--- app/Enums/TableStatus.php
+++ app/Enums/TableStatus.php
@@ -5,7 +5,7 @@ namespace App\Enums;
 enum TableStatus: string
 {
     case Pending = 'Pending';
-    case Avalaiable = 'Avalaiable';
-    case Unavalaiable = 'Unavalaiable';
+    case Available = 'Available';
+    case Unavailable = 'Unavailable';
     case Reverved = 'Reserved';
 }
